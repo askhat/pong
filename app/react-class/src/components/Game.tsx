7ed54f8f241bd9fe946f93ddcede5cdd7cbfc59c
Askhat Bikmetov <askhat.bikmetov@akvelon.com>
2020-06-28 12:47:42 +0300

    Add Game component and context

---
+++ app/react-class/src/components/Game.tsx
@@ -0,0 +1,38 @@
+import React, { Component, createRef } from "react";
+import { Context2D } from "../ctx";
+import {
+  VIEWPORT_WIDTH,
+  VIEWPORT_HEIGHT,
+} from "@lib/constants";
+
+interface GameState {
+  ball: Circ;
+  paddleTop: Rect;
+  paddleBottom: Rect;
+}
+
+export class Game extends Component<unknown, GameState> {
+  ref = createRef<HTMLCanvasElement>();
+  req: number = null!;
+
+  update = () => {
+    this.req = requestAnimationFrame(this.update);
+  };
+
+  componentDidMount() {
+    this.update();
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.req);
+  }
+
+  render() {
+    return (
+      <>
+        <canvas ref={this.ref} width={VIEWPORT_WIDTH} height={VIEWPORT_HEIGHT} />
+        <Context2D.Provider value={() => this.ref.current?.getContext("2d")!} />
+      </>
+    );
+  }
+}
